2d833364e5076b0e993fd7b27cd792e98def05f6
David Salgado <david.salgado@digital.justice.gov.uk>
2019-05-10 17:07:17 +0100

    Move checking code into NamespaceChecker class

--- check-namespace.rb
+++ check-namespace.rb
@@ -19,11 +19,65 @@ YAML_FILES = [
 # md5sum of the standard main.tf file
 MAIN_TF_MD5 = '3049b2f652c1e04ddf44a0ec05f47a64'
 
-class Report
-  attr_reader :namespace, :passes, :fails, :warnings
+class NamespaceChecker
+  attr_reader :namespace, :report
 
-  def initialize(namespace)
-    @namespace = namespace
+  def initialize(args)
+    @namespace = args.fetch(:namespace)
+    @report = args.fetch(:report, Report.new)
+  end
+
+  def run
+    check_directory
+    report.output
+  end
+
+  private
+
+  def check_directory
+    dir = "namespaces/live-1.cloud-platform.service.justice.gov.uk/#{namespace}"
+    if is_dir?(dir)
+      check_yaml_files(dir)
+    end
+
+    resources_dir = "#{dir}/resources"
+
+    if is_dir?(resources_dir)
+      check_main_tf(resources_dir)
+    end
+  end
+
+  def check_yaml_files(dir)
+    YAML_FILES.each do |f|
+      filename = "#{dir}/#{f}"
+      is_file?(filename)
+    end
+  end
+
+  def check_main_tf(dir)
+    filename = "#{dir}/main.tf"
+    if is_file?(filename)
+      if Digest::MD5.hexdigest(File.read(filename)) == MAIN_TF_MD5
+        report.add_pass("#{filename} MD5 matches expected")
+      else
+        report.add_warning("#{filename} MD5 does not match expected")
+      end
+    end
+  end
+
+  def is_dir?(dir)
+    FileTest.directory?(dir) ? report.add_pass("#{dir} is a directory") : report.add_fail("#{dir} is not a directory")
+  end
+
+  def is_file?(file)
+    FileTest.exists?(file) ? report.add_pass("#{file} exists") : report.add_fail("#{file} does not exist")
+  end
+end
+
+class Report
+  attr_reader :passes, :fails, :warnings
+
+  def initialize
     @passes = []
     @fails = []
     @warnings = []
@@ -68,11 +122,9 @@ class Report
 end
 
 def main(namespace)
-  report = Report.new namespace
   git_pull
   checkout_branch
-  analyse_namespace report
-  report.output()
+  NamespaceChecker.new(namespace: namespace).run
 end
 
 def git_pull
@@ -81,49 +133,6 @@ end
 
 def checkout_branch
   puts 'TODO checkout_branch...'
-end
-
-def analyse_namespace(report)
-  puts "analysing namespace #{report.namespace}"
-  check_directory(report)
-end
-
-def check_directory(report)
-  dir = "namespaces/live-1.cloud-platform.service.justice.gov.uk/#{report.namespace}"
-  if is_dir?(report, dir)
-    check_yaml_files(report, dir)
-  end
-
-  resources_dir = "#{dir}/resources"
-  if is_dir?(report, resources_dir)
-    check_main_tf(report, resources_dir)
-  end
-end
-
-def check_yaml_files(report, dir)
-  YAML_FILES.each do |f|
-    filename = "#{dir}/#{f}"
-    is_file?(report, filename)
-  end
-end
-
-def check_main_tf(report, dir)
-  filename = "#{dir}/main.tf"
-  if is_file?(report, filename)
-    if Digest::MD5.hexdigest(File.read(filename)) == MAIN_TF_MD5
-      report.add_pass("#{filename} MD5 matches expected")
-    else
-      report.add_warning("#{filename} MD5 does not match expected")
-    end
-  end
-end
-
-def is_dir?(report, dir)
-  FileTest.directory?(dir) ? report.add_pass("#{dir} is a directory") : report.add_fail("#{dir} is not a directory")
-end
-
-def is_file?(report, file)
-  FileTest.exists?(file) ? report.add_pass("#{file} exists") : report.add_fail("#{file} does not exist")
 end
 
 main ARGV.shift
